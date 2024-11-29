b7bd5e4b400591693eec9fd6b5f2cc20f756e369
WongYC-66 <yengchien@hotmail.com>
2024-11-29 14:16:49 +0800

    update : FE api_url

--- app/javascript/layout/API_URL.jsx
+++ app/javascript/layout/API_URL.jsx
@@ -1,5 +1,5 @@
 // API_URL.js
 // const API_URL = "http://localhost:3002"
-const API_URL = "http://localhost:3000"
-// const API_URL = "https://odin-messaging-app-be.adaptable.app"
+// const API_URL = "http://localhost:3000"
+const API_URL = "https://odin-rails-messaging-app.onrender.com"
 export default API_URL;
